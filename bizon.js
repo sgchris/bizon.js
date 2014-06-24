b74ccc84a6eabcee2e62796ed510784ff1777f94
Gregory C. <sgchris@gmail.com>
2014-06-24 10:16:41 +0300

    A few fixes from JSHint
    http://www.jshint.com/

--- bizon.js
+++ bizon.js
@@ -1,5 +1,5 @@
 ;(function() {
-	
+
 	// helping tools
 	var tools = {
 		// clear the dom element content
@@ -8,22 +8,24 @@
 				DOMElem.removeChild(DOMElem.firstChild);
 			}
 		},
-		
+
 		createElement: function (tag, attrs, content, isHtml) {
 			// create the element
 			var newElem = document.createElement(tag);
-			
+
 			// set attributes
 			if (attrs && attrs instanceof Object) {
 				for (var attrName in attrs) {
-					// validate value type
-					var valueType = typeof(attrs[attrName]);
-					if (['string', 'number'].indexOf(valueType) >= 0) {
-						newElem.setAttribute(attrName, attrs[attrName]);
-					}
+          if (typeof(attrs[attrName]) !== 'function') {
+            // validate value type
+            var valueType = typeof(attrs[attrName]);
+            if (['string', 'number'].indexOf(valueType) >= 0) {
+              newElem.setAttribute(attrName, attrs[attrName]);
+            }
+          }
 				}
 			}
-			
+
 			// set the content
 			if (content) {
 				if (content instanceof HTMLElement) {
@@ -34,32 +36,32 @@
 					newElem.textContent = content;
 				}
 			}
-			
+
 			return newElem;
 		},
-		
+
 		getElementIndex: function (elem) {
 			var i = 0;
-			while( (elem = elem.previousSibling) != null ) i++;
+			while( (elem = elem.previousSibling) !== null ) i++;
 			return i;
 		},
-		
+
 		getElementRelativeTop: function(elem) {
 			return elem.offsetTop - elem.parentNode.offsetTop;
 		}
 	};
-	
+
 	// define bizon object
 	var bizonObj = function(container) {
 		// private configurable // 
-		
+
 		// space between each small image and its wrapper
 		this._smallImagesPadding = 3;
 		// padding of the body
 		this._windowPadding = 15;
 
 		// private //
-		
+
 		this._bigImage = null; // IMG
 		this._bigImageTitle = null; // DIV
 		this._bigImageWrapper = null; // DIV (>IMG)
@@ -67,22 +69,22 @@
 		this._currentImage = 0;
 
 		// public // 
-		
+
 		this.container = container;
 		this.images = [];
 		this.loadImages();
 		this.buildDom();
 		this.bindEvents();
 		this.fixSize();
-		
+
 		// initializers 
-		
+
 		var that = this;
 		window.addEventListener('resize', function() {
 			that.fixSize();
 		}, false);
 	};
-	
+
 	bizonObj.prototype = {
 		// function(scrollTo, elem)
 		animateScrollTo: (function() {
@@ -95,62 +97,60 @@
 				var initialScrollTop = elem.scrollTop;
 				var maxScrollTop = elem.scrollHeight - elem.clientHeight;
 				var delta = Math.floor((finalScrollTo - initialScrollTop) / destinationDivide);
-				if (delta == 0 || (initialScrollTop >= maxScrollTop && finalScrollTo >= maxScrollTop)) {
+				if (delta === 0 || (initialScrollTop >= maxScrollTop && finalScrollTo >= maxScrollTop)) {
 					return;
 				}
 
 				var nextScrollTop = initialScrollTop + delta;
 				elem.scrollTop = nextScrollTop;
 
-				var that = this;
 				setTimeout(function() {
 					animate(elem);
 				}, timeoutInterval);
-			}
+			};
 
 			return function(scrollTo, elem) {
 				finalScrollTo = scrollTo;
 				animate(elem);
-			}
+			};
 		})(),
 		// function(newHeight, elem)
 		animateHeightTo: (function() {
-			
+
 			var timeoutInterval = 10;
 			var destinationDivide = 3;
 			var finalHeight;
-			var winHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-			
+
 			var animate = function(elem, callbackFn) {
 				var initialHeight = elem.clientHeight;
 				var delta = Math.floor((finalHeight - initialHeight) / destinationDivide);
-				if (delta == 0) {
+				if (delta === 0) {
 					if (typeof(callbackFn) == 'function') callbackFn();
 					return;
 				}
-				
+
 				elem.style.height = (initialHeight + delta) + 'px';
-				
+
 				setTimeout(function() {
 					animate(elem, callbackFn);
 				}, timeoutInterval);
-			}
-			
+			};
+
 			return function(newHeight, elem, callbackFn) {
 				finalHeight = newHeight;
 				animate(elem, callbackFn);
-			}
+			};
 		})(),
 		// function(newHeight, elem)
 		fadeOut: (function() {
-			
+
 			var elemOpacity = 1;
 			var timeoutInterval = 10;
 			var _fadeOut = function(elem, callbackFn) {
 				if (elemOpacity >= 0) {
 					elemOpacity-= 0.2;
 					elem.style.opacity = elemOpacity;
-					
+
 					setTimeout(function() {
 						_fadeOut(elem, callbackFn);
 					}, timeoutInterval);
@@ -159,17 +159,17 @@
 						callbackFn();
 					}
 				}
-			}
-			
+			};
+
 			return function(elem, callbackFn) {
 				elemOpacity = 1;
 				_fadeOut(elem, callbackFn);
-			}
+			};
 		})(),
 		// load images into an object
 		loadImages: function() {
 			this.images = [];
-			
+
 			var that = this;
 			[].forEach.call(this.container.querySelectorAll('img'), function(img) {
 				that.images.push({
@@ -187,10 +187,9 @@
 		buildDom: function() {
 			if (this.container.classList.contains('bizon-initialized')) return;
 			tools.empty(this.container);
-			
-			var albumTitle = this.container.getAttribute('title');
+
 			// big image
-			this._bigImage = tools.createElement('img', {'src':this.images[this._currentImage]['fullImageSrc']});
+			this._bigImage = tools.createElement('img', {'src':this.images[this._currentImage].fullImageSrc});
 			this._bigImageWrapper = tools.createElement('div', {'class':'bizon-image-wrapper'}, this._bigImage);
 			this._bigImageWrapper.appendChild(tools.createElement('div', {'class': 'bizon-album-title'}, this.container.getAttribute('title') || 'Gallery'));
 			this._bigImageTitle = tools.createElement('div', {'class': 'bizon-image-title'}, 'Image description goes here...');
@@ -199,13 +198,13 @@
 			this._bigImageWrapper.appendChild(tools.createElement('div', {'class': 'bizon-arrow-left', 'title': 'Previous image'}, '<'));
 			this._bigImageWrapper.appendChild(tools.createElement('div', {'class': 'bizon-full-screen', 'title': 'Full screen'}, '#'));
 			this._bigImageWrapper.appendChild(tools.createElement('div', {'class': 'bizon-close', 'title': 'Close'}, 'x'));
-			
+
 			// small images
 			this._smallImagesWrapper = tools.createElement('div', {'class':'bizon-small-images-wrapper'});
 			for (var i=0, totalImages=this.images.length; i<totalImages; i++) {
 				var smallImageWrapper = tools.createElement('div', {'class':'bizon-small-image-wrapper'});
-				if (i==0) smallImageWrapper.classList.add('bizon-active');
-				
+				if (i===0) smallImageWrapper.classList.add('bizon-active');
+
 				var smallImage = tools.createElement('img', {
 					'src': this.images[i].src,
 					'class': 'bizon-small-image'
@@ -213,7 +212,7 @@
 				smallImageWrapper.appendChild(smallImage);
 				this._smallImagesWrapper.appendChild(smallImageWrapper);
 			}
-			
+
 			// add to container
 			this.container.appendChild(this._bigImageWrapper);
 			this.container.appendChild(this._smallImagesWrapper);
@@ -226,19 +225,19 @@
 		setActiveImage: function(imgNumber) {
 			if (typeof(imgNumber) == 'undefined') imgNumber = this._currentImage; 
 			this._currentImage = imgNumber;
-			
+
 			// remove `active` 
 			var currentActiveElem = this._smallImagesWrapper.querySelector('.bizon-active');
 			if (currentActiveElem) currentActiveElem.classList.remove('bizon-active');
-			
+
 			// set active;
 			var theSmallImage = this._smallImagesWrapper.querySelectorAll('div.bizon-small-image-wrapper')[imgNumber]; 
 			theSmallImage.classList.add('bizon-active');
-			this._bigImage.src = this.images[imgNumber]['fullImageSrc'];
+			this._bigImage.src = this.images[imgNumber].fullImageSrc;
 
 			// set image title
 			this._bigImageTitle.textContent = this.images[this._currentImage].alt || this.images[this._currentImage].alt; 
-			
+
 			// fix small images scroll
 			// if below
 			var smallImageScrollTop = tools.getElementRelativeTop(theSmallImage);
@@ -251,33 +250,33 @@
 				// this._smallImagesWrapper.scrollTop = theSmallImage.offsetTop;
 				this.animateScrollTo(smallImageScrollTop, this._smallImagesWrapper);
 			}
-			
+
 			// hide arrows
-			if (this._currentImage == 0) {
+			if (this._currentImage === 0) {
 				this.container.getElementsByClassName('bizon-arrow-left')[0].style.visibility = 'hidden';
 			} else {
 				this.container.getElementsByClassName('bizon-arrow-left')[0].style.visibility = 'visible';
 			}
-			
+
 			if (this._currentImage >= this.images.length - 1) {
 				this.container.getElementsByClassName('bizon-arrow-right')[0].style.visibility = 'hidden';
 			} else {
 				this.container.getElementsByClassName('bizon-arrow-right')[0].style.visibility = 'visible';
 			}
-			
+
 			this._bigImage.style.opacity = 1;
 			this.fixSize();
 		},
 		// fix elements sized according to the image/screen
 		fixSize: function() {
 			var that = this;
-			
+
 			// get full width/height
 			var containerWidth = this.container.clientWidth;
 			var containerHeight = this.container.clientHeight;
 			var bigImageWrapperWidth = Math.floor(containerWidth * 0.85) - 20;
 			var bigImageWrapperRatio = bigImageWrapperWidth / containerHeight;
-			
+
 			// fix big image wrapper
 			this._bigImageWrapper.style.width = bigImageWrapperWidth + 'px';
 			this._bigImageWrapper.style.height = containerHeight + 'px';
@@ -296,7 +295,7 @@
 
 			// fix main image
 			var currentImage = this.images[this._currentImage];
-			var bigImageRatio = currentImage['ratio'];
+			var bigImageRatio = currentImage.ratio;
 			if (bigImageRatio > bigImageWrapperRatio) {
 				// set max width
 				this._bigImage.style.width = bigImageWrapperWidth + 'px';
@@ -350,10 +349,10 @@
 		// bind gallery events
 		bindEvents: function() {
 			var that = this;
-			
+
 			// click small image (wrapper)
 			[].forEach.call(this.container.querySelectorAll('.bizon-small-image-wrapper'), function(imgWrapper) {
-				imgWrapper.addEventListener('click', function(event) {
+				imgWrapper.addEventListener('click', function() {
 					var elem = imgWrapper;
 					if (elem.classList.contains('bizon-small-image-wrapper')) {
 						var imgNumber = tools.getElementIndex(elem);
@@ -361,41 +360,41 @@
 					}
 				});
 			});
-			
+
 			// click "next"
 			that.container.getElementsByClassName('bizon-arrow-right')[0].addEventListener('click', function() {
 				that.nextImage();
 			});
-			
+
 			// click "prev"
 			that.container.querySelector('.bizon-arrow-left').addEventListener('click', function() {
 				that.prevImage();
 			});
-			
+
 			// click on main image 
 			that.container.querySelector('.bizon-image-wrapper img').addEventListener('click', function() {
 				that.nextImage();
 			});
-			
+
 			// click on "close"
 			that.container.querySelector('.bizon-close').addEventListener('click', function() {
 				that.close();
 			});
-			
-			
+
+
 		}
 	};
-	
+
 	// define main bizon function
 	window.bizon = function(container) {
 		return new bizonObj(container);
 	};
-	
+
 	// initialize bizon galleries - all elements with class "bizon"
 	document.addEventListener('DOMContentLoaded', function() {
 		[].forEach.call(document.querySelectorAll('.bizon'), function(bizonContainer) {
-			bizon(bizonContainer);
+			window.bizon(bizonContainer);
 		});
 	}, false);
-	
+
 })();
